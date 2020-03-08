ee2b7a2e7dd9a182fdcc4932638c24ed077d25f3
Marko Smilevski <m.smilevski57@gmail.com>
2020-03-08 01:02:33 +0100

    Update detector and recognizer for mixed precission

--- keras_ocr/recognition.py
+++ keras_ocr/recognition.py
@@ -200,7 +200,7 @@ def build_model(alphabet,
     """
     assert len(filters) == 7, '7 CNN filters must be provided.'
     assert len(rnn_units) == 2, '2 RNN filters must be provided.'
-    inputs = keras.layers.Input((height, width, 3 if color else 1))
+    inputs = keras.layers.Input((height, width, 3 if color else 1), dtype='float32')
     x = keras.layers.Permute((2, 1, 3))(inputs)
     x = keras.layers.Lambda(lambda x: x[:, :, ::-1])(x)
     x = keras.layers.Conv2D(filters[0], (3, 3), activation='relu', padding='same', name='conv_1')(x)
@@ -281,6 +281,7 @@ def build_model(alphabet,
     x = keras.layers.Dense(len(alphabet) + 1,
                            kernel_initializer='he_normal',
                            activation='softmax',
+                           dtype='float32',
                            name='fc_12')(x)
     x = keras.layers.Lambda(lambda x: x[:, rnn_steps_to_discard:])(x)
     model = keras.models.Model(inputs=inputs, outputs=x)
