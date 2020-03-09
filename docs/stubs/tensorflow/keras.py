e25f11be3e0fc8dc4422492f391d60c2a32dc1a2
Fausto Morales <faustomorales@gmail.com>
2020-03-08 21:39:11 -0500

    Add TensorFlow library stubs to fix readthedocs builds.

---
+++ docs/stubs/tensorflow/keras.py
@@ -0,0 +1,22 @@
+class Object():
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __getattr__(self, item):
+        return Object()
+
+    def __dir__(self):
+        return []
+
+    def __call__(self, *args, **kwargs):
+        return Object()
+
+    def __mro_entries__(self, _):
+        return (Object(), )
+
+
+backend = Object()
+layers = Object()
+models = Object()
+utils = Object()
+optimizers = Object()
