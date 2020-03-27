900f873346728f3079ee66ccb92f5e9a21c88e95
Fausto Morales <faustomorales@gmail.com>
2020-03-27 18:12:10 -0500

    Ensure that sequential spaces do not appear in input.

--- keras_ocr/recognition.py
+++ keras_ocr/recognition.py
@@ -374,6 +374,9 @@ class Recognizer:
             assert all(
                 len(sentence) <= max_string_length
                 for sentence in sentences), 'A sentence is longer than this model can predict.'
+            assert all("  " not in sentence for sentence in sentences), (
+                'Strings with multiple sequential spaces are not permitted. '
+                'See https://github.com/faustomorales/keras-ocr/issues/54')
             label_length = np.array([len(sentence) for sentence in sentences])[:, np.newaxis]
             labels = np.array([[self.alphabet.index(c)
                                 for c in sentence] + [-1] * (max_string_length - len(sentence))
