2d2c78b3966c06ffae595626437ed8e1591379b6
Fausto Morales <faustomorales@gmail.com>
2020-04-04 12:10:22 -0500

    Remove custom objects due to disuse and incompatibility with recent TensorFlow versions.

--- keras_ocr/__init__.py
+++ keras_ocr/__init__.py
@@ -1,5 +1,4 @@
-from . import (detection, recognition, tools, data_generation, pipeline, evaluation, datasets,
-               custom_objects)
+from . import (detection, recognition, tools, data_generation, pipeline, evaluation, datasets)
 
 from ._version import get_versions
 __version__ = get_versions()['version']
