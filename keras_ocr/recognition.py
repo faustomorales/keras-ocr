fa5a370a88aa2997a50085fca1a9915c807ab919
letco <ocomabo@outlook.com>
2021-06-30 12:50:22 +0300

    fix

--- keras_ocr/recognition.py
+++ keras_ocr/recognition.py
@@ -440,10 +440,12 @@ class Recognizer:
                 images = [sample[0] for sample in batch]
             images = np.array([image.astype("float32") / 255 for image in images])
             sentences = [sample[1].strip() for sample in batch]
+            
             if lowercase:
                 sentences = [sentence.lower() for sentence in sentences]
             else:
                 sentences = [sentence.upper() for sentence in sentences]
+                
             for c in "".join(sentences):
                 assert c in self.alphabet, "Found illegal character: {}".format(c)
             assert all(sentences), "Found a zero length sentence."
