79ade8fe4b2a3ff21e680ccfead8ecfdb30cc7b4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 10:39:34 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/train_recognizer_synthtext90_multigpu.py
+++ scripts/train_recognizer_synthtext90_multigpu.py
@@ -51,7 +51,26 @@ def download_extract_and_process_dataset(data_path):
     if not os.path.isdir(extraction_directory):
         print("Extracting files.")
         with tarfile.open(os.path.join(data_path, "mjsynth.tar.gz")) as tfile:
-            tfile.extractall(data_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tfile, data_path)
 
 
 def get_image_generator(filepaths, augmenter, width, height):
